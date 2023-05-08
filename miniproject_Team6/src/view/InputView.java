b979f4c876532117da81f73f55f91c982493a2e0
eycha <sts07129@naver.com>
2023-05-08 13:14:20 +0900

    회원가입 API 구현
    회원가입 API 구현

--- miniproject_Team6/src/view/InputView.java
+++ miniproject_Team6/src/view/InputView.java
@@ -2,22 +2,39 @@ package view;
 
 import java.util.Scanner;
 
+import controller.Controller;
+import model.dto.CustomerDTO;
+
 public class InputView {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	    static Scanner sc = new Scanner(System.in);
-	    private static final String WELCOME_MSG = "\n 번호를 선택해주세요 \n"
-	            + "1. 회원주문\n"
-	            + "2. 비회원주문\n"
-	            + "3. 회원가입\n"
-	            + "Q. 종료";
-	    
-	    private static final String FIRST_MGS = "회원정보를 입력해주세요";
-	    private static final String SECOND_MGS = "주문하실 메뉴를 골라주세요.";
-	    private static final String THIRD_MGS = "회원가입 정보를 입력해주세요.";
 
+		String cust_id;
+		String name;
+		String cust_phone;
+		String cust_birth;
+		String gender;
+		int amount = 0;
+		int grade_id = 10;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("회원 아이디");
+		cust_id = sc.next();
+		System.out.println("이름");
+		name = sc.next();
+		System.out.println("전화번호");
+		cust_phone = sc.next();
+		System.out.println("생년월일");
+		cust_birth = sc.next();
+		System.out.println("성별");
+		gender = sc.next();
+
+		System.out.println("회원정보를 출력합니다.");
+		System.out.println(cust_id + " " + name + " " + cust_phone + " " + cust_birth + " " + gender + " " + amount
+				+ " " + grade_id);
+
+		Controller.addCustomer(new CustomerDTO(cust_id, name, cust_phone, cust_birth, gender, amount, grade_id));
 	}
 
 }
