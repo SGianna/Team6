7cece0bcb55340fad858043bf649d759805b73d2
dodhddl <dldodhd9@gmail.com>
2023-05-08 11:58:19 +0900

    Auto stash before rebase of "origin/develope"
    순서변경

--- miniproject_Team6/sql/cafeDDL.sql
+++ miniproject_Team6/sql/cafeDDL.sql
@@ -1,11 +1,9 @@
 
-
 use fisa;
 
 
 -- 고객 테이블
 drop table if exists customer;
-
 -- 등급 테이블
 drop table if exists grade;
 
@@ -16,6 +14,27 @@ drop table if exists menu;
 drop table if exists sale;
 
 
+
+create table grade(
+	grade_id int,
+	grade_name varchar(30),
+	standard int,
+	primary key (grade_id)		
+	);
+
+	desc grade;
+
+		
+create table menu(
+	menu_id int,
+	menu_name varchar(50),
+	price int,
+	categories int,
+	calorie int
+	);
+	
+	desc menu;
+	
 create table customer(
 	cust_id varchar(5),
 	cust_name varchar(45) not null,
@@ -27,30 +46,9 @@ create table customer(
     primary key(cust_id),
     foreign key (grade_id) references grade(grade_id)
 	);
-
-
-
-desc customer;
-
-select * from information_schema.TABLE_CONSTRAINTS tc
-where table_name='customer';
-
-create table grade(
-	grade_id int,
-	grade_name varchar(30),
-	standard int,
-	primary key (grade_id)		
-	);
-
-
 	
-create table menu(
-	menu_id int,
-	menu_name varchar(50),
-	price int,
-	categories int,
-	calorie int
-	);
+    desc customer;
+
 	
 create table sale(
 	order_id int,
@@ -64,45 +62,3 @@ create table sale(
 desc sale;
 
 
-
--- select * from information_schema.TABLE_CONSTRAINTS tc
--- where table_name='sale';
--- alter talbe
-
-desc customer;
-
-select * from information_schema.TABLE_CONSTRAINTS tc
-where table_name='customer';
-
-create table grade(
-	grade_id int,
-	grade_name varchar(30),
-	standard int,
-	primary key (grade_id)		
-	);
-
-
-	
-create table menu(
-	menu_id int,
-	menu_name varchar(50),
-	price int,
-	categories int,
-	calorie int
-	);
-	
-create table sale(
-	order_id int,
-	order_table timestamp,
-	total int,
-	cust_id varchar(5),
-	primary key (order_id),
-	foreign key (cust_id) references customer(cust_id)
-);
-
-desc sale;
-
-select * from information_schema.TABLE_CONSTRAINTS tc
-where table_name='sale';
-alter talbe
-alter table sale add foreign key (cust_id) references Cafecustomer(cust_id);
