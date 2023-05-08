90e372ad2537ef064fac1d7294def4c1e4796b3a
SGianna <yjeong1107@gmail.com>
2023-05-08 11:32:53 +0900

    Auto stash before rebase of "origin/develope"
    db에서 메뉴판 가져오는 메소드 구현

--- miniproject_Team6/src/controller/Controller.java
+++ miniproject_Team6/src/controller/Controller.java
@@ -1,5 +1,22 @@
 package controller;
 
+import java.sql.SQLException;
+
+import model.MenuDAO;
+import view.OutputView;
+
 public class Controller {
 
+	public static void getAllMenu() {
+		try {
+			
+			//OutputView.getMenuListView(MenuDAO.getAllMenu());
+		
+		}catch (SQLException s) {
+			s.printStackTrace();
+			OutputView.showError("메뉴판 생성에 실패하였습니다.");
+		}
+		
+		
+	}
 }
--- miniproject_Team6/src/view/OutputView.java
+++ miniproject_Team6/src/view/OutputView.java
@@ -2,4 +2,22 @@ package view;
 
 public class OutputView {
 
+//	public static void getMenuListView(ArrayList<MenuDTO> menuList) {
+//		int listSize = menuList.size();
+//		
+//		if(listSize != 0) {
+//			int count = 1;
+//			
+//			for(MenuDTO m : menuList) {
+//				System.out.println("메뉴판");
+//			}
+//		}else {
+//			System.out.println("검색 정보 0");
+//		}
+//	}
+
+	//예외 상황 출력
+	public static void showError(String message){
+		System.out.println(message);		
+	}
 }
