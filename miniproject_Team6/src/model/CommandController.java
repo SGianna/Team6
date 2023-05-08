64b4ba45b659cfd2ca37996f7c24c9ce35502cdc
Woori_Jaemin <jaemin.ko98@gmail.com>
2023-05-08 10:35:03 +0900

    Stash적용
    Stash적용 테스트 입니다.

---
+++ miniproject_Team6/src/model/CommandController.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.Arrays;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+
+public enum CommandController {
+    FIRST("1"),
+    SECOND("2"),
+    THIRD("3"),
+    QUIT("Q");
+
+    private static final String INVALID_COMMAND_MSG = "올바르지 않은 입력입니다.";
+    private final String command;
+
+    public static CommandController from(String command){
+        return Arrays.stream(values())
+                .filter(commandController -> commandController.command.equals(command))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_COMMAND_MSG));
+    }
+    public boolean isNotQuit() { return this != QUIT; }
+
+
+}
--- miniproject_Team6/src/view/InputView.java
+++ miniproject_Team6/src/view/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
 	            + "1. 회원주문\n"
 	            + "2. 비회원주문\n"
 	            + "3. 회원가입\n"
-	            + "4. 종료";
+	            + "Q. 종료";
 	    
 	    private static final String FIRST_MGS = "회원정보를 입력해주세요";
 	    private static final String SECOND_MGS = "주문하실 메뉴를 골라주세요.";
